9686c84a30d1defc35e7968483aa49f97e39e147
ziadmustafa1 <124303556+ziadmustafa1@users.noreply.github.com>
2023-08-22 13:09:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-/* eslint-disable import/newline-after-import */
-// index.js
-const express = require('express')
-const app = express()
-const PORT = 4000
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import http from "http";
+import mongoose from "mongoose";
+import "dotenv/config";
+import routes from "./src/routes/index.js";
 
+const app = express();
 
-app.get('/home', (req, res) => {
-  res.status(200).json('Welcome, your app is working well');
-})
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
+app.use("/api/v1", routes);
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+const port = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+  console.log("Mongodb connected");
+  server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
+}).catch((err) => {
+  console.log({ err });
+  process.exit(1);
 });
 
-// Export the Express API
-module.exports = app
+//test
